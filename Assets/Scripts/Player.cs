b98a4dc9db1c73699809d66898afc6ca653da254
HarryH <harry.heath93@gmail.com>
2023-04-22 01:39:12 +0200

    Fixed character script for flipping sprite

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Flip sprites horizontally based on input
+    private void Update()
     {
-        
-    }
+        // Get the horizontal input value
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Loop through each child object of the parent object
+        foreach(Transform child in transform)
+        {
+            // Get all childed sprite renderers
+            SpriteRenderer spriteRenderer = child.GetComponent<SpriteRenderer>();
+            if(spriteRenderer != null)
+            {
+                // Flip the sprite horizontally
+                if(horizontalInput < 0)
+                    spriteRenderer.flipX = false;
+                
+                else if(horizontalInput > 0)
+                    spriteRenderer.flipX = true;
+            }
+        }
     }
 }
