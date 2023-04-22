19f3fb3fe63324229ec31695140889313a5246ad
HarryH <harry.heath93@gmail.com>
2023-04-22 13:42:39 +0200

    Set up controller for isGrounded logic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,33 @@
+using System;
 using UnityEngine;
 public class Player : MonoBehaviour
 {
     public bool FlipWhenFacingRight;
+    private Rigidbody2D _rb;
+    public bool IsGrounded = true;
+    public Transform FeetPos;
+    public float CheckRadius;
+    public LayerMask GroundLayer;
+    
+    
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+
+
     private void Update()
     {
         // Get the horizontal input value
         float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        IsGrounded = Physics2D.OverlapCircle(FeetPos.position, CheckRadius, GroundLayer);
 
         // Loop through each child object of the parent object
         foreach(Transform child in transform)
