1b73f33e77990d20c6bd49275ddd1e27aa5bd6f5
Tistou Blomberg <tistou.blomberg@gmail.com>
2023-04-23 20:02:04 +0200

    bird version 1

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Entity;
+using UnityEngine;
+
+public class Bird : MonoBehaviour
+{
+    public AudioClip _enter;
+    public AudioClip _die;
+    public AudioClip _eat;
+    public AudioSource _audio;
+    public float _speed = 5f;
+    public int Damage = 1;
+    public Rigidbody2D _rigidbody2D;
+
+    private void OnEnable()
+    {
+        if (_enter != null)
+        {
+            _audio.clip = _enter;
+            _audio.Play();
+        }
+        
+        Transform target = FindObjectOfType<PlayerController>().transform;
+
+        // Calculate direction vector towards target
+        Vector2 direction = target.transform.position - transform.position;
+        direction.Normalize();
+
+        // Add velocity towards target
+        _rigidbody2D.velocity = direction * _speed;
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (gameObject.GetComponent<Health>().IsDead)
+            return;
+
+        if (other.gameObject.CompareTag("Enemy"))
+            return;
+        
+        PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+        
+        if (player != null && !(player is null))
+        {
+            if (_eat != null)
+            {
+                _audio.clip = _eat;
+                _audio.Play();
+            }
+            Debug.Log("BIRD ATE TOAD!");
+            other.gameObject.GetComponent<IDamageable>().ModifyHealth(-Damage);
+
+        }
+    }
+    
+    private void Die()
+    {
+        if (_die != null)
+        {
+            _audio.clip = _die;
+            _audio.Play();
+            Destroy(gameObject, _audio.clip.length);
+        }
+    }
+}
