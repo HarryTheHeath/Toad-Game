3a043fdc764d574a423d6a15a82854cf353184f8
HarryH <harry.heath93@gmail.com>
2023-04-23 19:03:58 +0200

    Added wave buffer to new spawn system

--- Assets/Scripts/Waves/NewWaveSpawner.cs
+++ Assets/Scripts/Waves/NewWaveSpawner.cs
@@ -9,17 +9,18 @@ public class NewWaveSpawner : MonoBehaviour
     public GameObject[] enemyTypes;
     public Transform[] SpawnPoints;
     public float TimeBetweenSpawns;
+    public float WaveBuffer = 3f;
     private float NextSpawnTime;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    void Start() => NextSpawnTime += WaveBuffer;
 
     // Update is called once per frame
     void Update()
     {
+        if (Time.time < WaveBuffer)
+            return;
+        
         if (Time.time > NextSpawnTime)
         {
             Instantiate(enemyTypes[0], SpawnPoints[Random.Range(0, SpawnPoints.Length)].position,
